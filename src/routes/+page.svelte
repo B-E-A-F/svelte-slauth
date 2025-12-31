80c6f73dabc1284d03f1915e506c1fcc96f18c1e
bea <bea@a4r.dev>
2025-12-30 18:03:23 -0800

    ✨ Add initial template

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex items-center justify-center h-screen bg-base-300">
+  <div class="card card-side bg-base-100 shadow-sm">
+    <figure>
+      <img
+        src="https://avatars.githubusercontent.com/u/6097418?s=200&v=4"
+        alt="Movie" />
+    </figure>
+    <div class="card-body">
+      <h2 class="card-title">New movie is released!</h2>
+      <p>Click the button to watch on Jetflix app.</p>
+      <div class="card-actions justify-end">
+        <button class="btn btn-primary">Watch</button>
+      </div>
+    </div>
+  </div>
+</div>
