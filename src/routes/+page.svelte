97c75c8ac638b90c147d31d869bc71ae62032131
bea <bea@a4r.dev>
2026-01-05 10:39:03 -0800

    🐛 Fix linting errors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -69,14 +69,14 @@
 		<fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
 			<legend class="fieldset-legend">Login</legend>
 
-			<label class="label">Email</label>
-			<input type="email" class="input" placeholder="Email" />
+			<label class="label" for="email">Email</label>
+			<input id="email" type="email" class="input" placeholder="Email" />
 
-			<label class="label">Password</label>
-			<input type="password" class="input" placeholder="Password" />
+			<label class="label" for="password">Password</label>
+			<input id="password" type="password" class="input" placeholder="Password" />
 
-			<label class="label">Authenticator</label>
-			<input type="text" class="input" placeholder="Authenticator" />
+			<label class="label" for="authenticator">Authenticator</label>
+			<input id="authenticator" type="text" class="input" placeholder="Authenticator" />
 
 			<button class="btn btn-neutral mt-4">Login</button>
 		</fieldset>
